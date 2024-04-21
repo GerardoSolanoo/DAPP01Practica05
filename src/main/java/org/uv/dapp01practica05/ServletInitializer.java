f97a216758d5682d077dccd8d9522ef100a50230
GerardoSolanoo <gerardosolano.contact@gmail.com>
2024-04-21 11:14:59 -0600

    base code

---
+++ src/main/java/org/uv/dapp01practica05/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.uv.dapp01practica05;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Dapp01Practica05Application.class);
+    }
+
+}
